b880ab8332b9d7d7f1fefe167434c7d1a352c47f
Rich Owen <richowen87@icloud.com>
2025-10-24 17:26:03 +0100

    basic knock up of whole site

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ const hamMenu = document.querySelector(".ham-menu");
 
 const offScreenMenu = document.querySelector(".off-screen-menu");
 
+const menuLinks = document.querySelectorAll(".off-screen-menu a");
+
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
 });
 
 
+menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      hamMenu.classList.remove("active");
+      offScreenMenu.classList.remove("active");
+    });
+  });
